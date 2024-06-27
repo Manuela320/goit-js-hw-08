41c122d7b6ec6e3c1a8eccef6a69023aeba8b511
Manuela320 <manuelaandricioaei63@gmail.com>
2024-06-27 16:46:09 +0300

    Added simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const listElement = document.querySelector('.gallery');
+const photosMarkup = createGalleryItem(galleryItems);
+
+function createGalleryItem(element) {
+    return element
+    .map(({preview, original, description}) => {
+        return `<a class='gallery_link' href="${preview}">
+        <img class="gallery_image" src=${original} alt=${description}/>
+        </a>`;
+    })
+    .join('');
+}
+
+listElement.insertAdjacentHTML("beforeend", photosMarkup);
+const galleryHandler = new SimpleLightbox('.gallery a', { 
+    captionsData: "alt", 
+    captionDelay: 250,
+ });
+ galleryHandler.on("show.simplelightbox")
