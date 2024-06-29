21b33a9bc54b767d74a0f3af2a06b4dcf53c2040
Manuela320 <manuelaandricioaei63@gmail.com>
2024-06-29 21:34:47 +0300

    video & feedback

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+const STORAGE_KEY ='videoplayer-current-time';
+const savedTime = localStorage.getItem(STORAGE_KEY);
+
+if (savedTime !== null) {
+    player.setCurrentTime(parseFloat(savedTime));
+}
+
+player.on('timeupdate', throttle(({ seconds }) => {
+    localStorage.setItem(STORAGE_KEY, seconds);
+}, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (savedData) {
+        form.email.value = savedData.email || '';
+        form.message.value = savedData.message || '';
+    }
+});
+
+const saveFormData = throttle(() => {
+    const formData = {
+        email: form.email.value,
+        message: form.message.value,
+    };
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}, 500);
+
+form.addEventListener('input', saveFormData);
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = {
+        email: form.email.value,
+        message: form.message.value,
+    };
+
+    console.log(formData);
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+});
